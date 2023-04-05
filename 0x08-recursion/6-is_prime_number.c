6ef833f6d34e9c60b567a6e68f2b5652d5dde40a
Joel Anang <jo.erl444@gmail.com>
2023-04-05 15:24:27 -0700

    Task 6 betty fixed

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * is_priime - identifies  if a number is a prime number.
+ * is_prime - identifies  if a number is a prime number.
  * @n: input number.
  * @c: iterator.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
