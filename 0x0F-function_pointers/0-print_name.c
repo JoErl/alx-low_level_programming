7b240ce55e12eb63e93f8da48620bd21651d4f0e
Joel Anang <jo.erl444@gmail.com>
2023-04-20 11:36:38 -0700

    Prototypes

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- * print_name -a function that prints a name
+ * print_name - a function that prints a name
  * @name: the name given
  * @f: function of the name
  *
