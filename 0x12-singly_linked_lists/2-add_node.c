7217c98c9e381013e2bcd7da50c03eeeed3161dd
Joel Anang <jo.erl444@gmail.com>
2023-04-28 05:32:12 -0700

    a function that adds a new node at the beginning of a list_t list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,41 +1,29 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
- * _strlen - function that returns the length of a string.
- * @s : s is a character
- * Return: value is i
- */
-int _strlen(const char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * add_node - this adds a new node at beginning of a list_t list.
+ * add_node - add a new node at beginning of a list_t list.
  * @head: head of a list_t list.
  * @str: value to insert into element.
  * Return: the number of nodes.
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *add;
+	list_t *new_node;
 
-	add = malloc(sizeof(list_t));
-	if (add == NULL)
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
 		return (NULL);
-	add->str = strdup(str);
+	}
 
-	add->len = _strlen(str);
-	add->next = *head;
-	*head = add;
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
 
-	return (add);
+	return (new_node);
+}
